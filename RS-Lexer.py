ce5b97c42ccb8e185407b27ac22a3f5b2f554a6c
shang <trishaannmonzon@gmail.com>
2024-12-11 03:51:45 +0800

    DELIMS

--- RS-Lexer.py
+++ RS-Lexer.py
@@ -84,6 +84,42 @@ class TokenType:
     ESCAPE_BACKSLASH = "ESCAPE_BACKSLASH"
     ESCAPE_QUOTE = "ESCAPE_QUOTE"
 
+    #DELIMS haha
+    DELIMS = {
+    'return': {'0', ' '},
+    'data_type': {' ', '~'},
+    'dynasty': {' '},
+    'mirror': {'|', '&', ']', ',', ' ', '}', ')', '~', '\n'},
+    'conditional': {'(', ' '},
+    'io': {'(', ' '},
+    'castle': {' '},
+    'int_float': {',', ' ',['general_operator'], ')', '~', '!', r'&', '|', '>', '<', '='},
+    'string': {' ', ')', ',', '&', '}', '~', '!', '='},
+    'assign_delim': {['alpha'], ['number'], '{', ' ', '-', '(', '"'},
+    'operator_delim': {['alpha'], ['number'], ' ', '-', '(', '{'},
+    'logical_delim': {'"', ['alpha'], ['number'], ' ', '-', '(', '{'},
+    'string_parts': {'"', ['alpha'], ['number'], ' ', '-', '(', '|', '&'},
+    'open_brace': {'{', '}', '(', ['number'], ' ', '"', ['alpha'], '\n', '>', '-'},
+    'close_brace': {'{', '}', '.', '~', ' ', ',', ')', '\n', '>', '&', ['general_operator'], '!', '|'},
+    'open_parenthesis': {'{', ['number'], ['alpha'], ' ', '-', '\n', '>', '(', ')', '"'},
+    'id': {'{', '\n', ' ', '~', ',', '(', ')', '[', ']', '}', ['general_operator'], '!', r'&', '|', '.'},
+    'close_parenthesis': {'{', ' ', ['general_operator'], '!', '&', '|', '\n', '~', '>', '.', ',', ')', '(', '[', ']', '}'},
+    'open_bracket': {']', ['number'], '-', ['alpha'], '(', ' ', '\n'},
+    'double_open_bracket': {' ', '\n', ['alpha'], '>'},
+    'close_bracket': {'\n', '(', ' ', '~', ',', ')', '[', ']', '}', ['general_operator'], '!', r'&', '|', '.', '\n'},
+    'double_close_bracket': {']',' ', '\n', ['alpha'], '>'},
+    'unary': {'|', '~', ')', ['general_operator'], '!', ' ', '\n'},
+    'concat': {' ', '"', ['alpha'], ['number'], '(', '{', '\n'},
+    'line': {'\n', ' ', ['alpha'], ']'},
+    'comma': {['alpha'], ' ', ['number'], '"', '-', '\n', '>', '{'},
+    'dot_op': {['alpha'], '[', '(', '\n'},
+    'nuww': {' ', '~', ')', '}', ',', '=', '\n', '!', '|', '&'},
+    'whitespace': {' ', '\n'},
+    'single_line_comment': {'\n'},
+    'all': {None}
+}
+
+
 
 class RoyalScriptLexer:
     def __init__(self, code):
